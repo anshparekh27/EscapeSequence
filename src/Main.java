84586983c5d499b5521b254920f90f9032146bde
anshp <anshparekh03@gmail.com>
2023-06-23 11:46:44 +0530

    escape

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Myself \"Ansh\" \"Parekh\"");
+
+        System.out.println("Hi, \n Good Morning !! ");
+
+        System.out.println("C:\\xyz\\abc.txt");
+        }
+    }
